0bb29ac0efd1c9486b8e83d65ec50d109dec8fb2
fernandez-julian <julifersan@hotmail.com>
2017-06-16 10:23:01 -0300

    version prueba dic

---
+++ TPO_1C2017/src/implementacion/DMediciones.java
@@ -0,0 +1,59 @@
+package implementacion;
+
+import tda.ColaTDA;
+import tda.ConjuntoMesesTDA;
+import tda.ConjuntoTDA;
+import tda.DMedicionesTDA;
+
+public class DMediciones implements DMedicionesTDA {
+
+	@Override
+	public void inicializar() {
+		int clave;
+		int [] valores;
+		int cantValores;
+	}
+
+	@Override
+	public void agregar(int anio, int mes, int dia, float medicion) {
+	
+
+	}
+
+	@Override
+	public void eliminarAnio(int anio) {
+	
+
+	}
+
+	@Override
+	public void eliminarMes(int dia) {
+	
+
+	}
+
+	@Override
+	public void eliminarDia(int anio, int mes, int dia) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ConjuntoTDA anios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ConjuntoMesesTDA meses(int anio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ColaTDA mediciones() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
