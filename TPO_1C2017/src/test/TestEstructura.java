e0204e572fa3d9296aa6c17457370a71d825e3e4
fernandez-julian <julifersan@hotmail.com>
2017-06-26 17:06:56 -0300

    Update TestEstructura.java

--- TPO_1C2017/src/test/TestEstructura.java
+++ TPO_1C2017/src/test/TestEstructura.java
@@ -11,7 +11,7 @@ public class TestEstructura {
 	 * Este main es para mediciones en float
 	 * 
 	 * */
-	public static void main(String[] Args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//PrecipitacionesTDA mediciones = null;
 		// crear la estructura 
